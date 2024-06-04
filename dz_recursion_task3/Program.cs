bcc038f0d5508118b3cbb596f7cbf40fdbc99fa7
Ekaterina.Tihonovich <Antipova_Katrin@mail.ru>
2024-06-04 21:57:35 +0300

    Решила task3

---
+++ dz_recursion_task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+
+void ShowArrayConversly(int[] arr, int i = 0)
+{
+    if (i == arr.Length)
+    {
+        return;
+    }
+    ShowArrayConversly(arr, i + 1);
+    Console.Write($"{(arr[i])} ");
+}
+
+int [] array = {9, 45, -7, 32, 897, -65};
+ShowArrayConversly(array);
