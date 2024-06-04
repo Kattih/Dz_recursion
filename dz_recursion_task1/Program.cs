2591baf7472b6c1bc5d7afd263478d171c065179
Ekaterina.Tihonovich <Antipova_Katrin@mail.ru>
2024-06-04 21:37:26 +0300

    Решила task1

---
+++ dz_recursion_task1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+bool CheckNatural(int num)
+{
+    if (num <= 0)
+    {
+        Console.WriteLine("Число не является натуральным");
+        return true;
+    }
+    return false;
+}
+
+void OutNatureNumbers(int m, int n)
+{
+    
+    {
+            if (m < n)
+        {
+            if (n == m-1)
+            return;
+            OutNatureNumbers(m, n-1);
+        }
+        
+        if (m > n)
+        {
+            if (n == m+1)
+            return;
+            OutNatureNumbers(m, n+1);
+        }
+        Console.Write($"{n} ");
+    }
+}
+
+Console.Write("Введите число M: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (CheckNatural(num1)) return;
+
+Console.Write("Введите число N: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (CheckNatural(num2)) return;
+
+OutNatureNumbers(num1, num2);
